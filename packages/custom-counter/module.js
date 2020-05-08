0cab83695f2476177a79ed82bcd21ed6d9daef1e
Nikola Zeravcic, local DEV (BTNTPC-001) <support@bytenet.rs>
2020-05-08 21:51:05 +0200

    Building out the module

--- packages/custom-counter/module.js
+++ packages/custom-counter/module.js
@@ -1,9 +1,25 @@
+const { resolve, join } = require('path')
+
 export default function (moduleOptions) {
+  // get all options for the module
   const options = {
     ...moduleOptions,
     ...this.options.customCounter
   }
-  console.log('options:', options)
+
+  // expose the namespace / set a default
+  if (!options.namespace) {
+    options.namespace = 'customCounter'
+  }
+  const { namespace } = options
+
+  // add the debug plugin
+  this.addPlugin({
+    src: resolve(__dirname, 'debug.js'),
+    // ** NOTE: we now use namespace here: **
+    fileName: join(namespace, 'debug.js'),
+    options // just pass through the options object
+  })
 }
 
 module.exports.meta = require('./package.json')
