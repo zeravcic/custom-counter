c29cda7c679db7ef0f83a19d340b9ccbc0c94e31
Nikola Zeravcic, local DEV (BTNTPC-001) <support@bytenet.rs>
2020-05-12 18:02:18 +0200

    style: lint

--- packages/custom-counter/store/modules/counter.js
+++ packages/custom-counter/store/modules/counter.js
@@ -1,14 +1,17 @@
 export default options => ({
   namespaced: true,
+
   state: () => ({
     options,
     count: options.initialValue
   }),
+
   mutations: {
-    adjust(state, data) {
+    adjust (state, data) {
       state.count += data
     }
   },
+
   getters: {
     count: state => state.count
   }
